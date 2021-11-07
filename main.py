c9f9bb361b855906f6204606dadfd182a8110628
Guilherme Soares <69168852+gsoso01@users.noreply.github.com>
2021-11-06 21:15:06 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+#################################################################################
+##   Projeto desenvolvido para a turma de IA048 - 2s2020 - UNICAMP - FEEC      ##
+##   Alunos: Guilherme Soares, Wesna Simone e Lucas Yagui                      ##
+##   Utilize uma interface como PyCharm para executar o arquivo                ##
+##   Modelo com 95% de precisão utilizando LIBRAS                              ##
+##   Letras Disponíveis: C,E,F,I,M,N,O,R,S                                     ##
+##   Coloque a mão no dentro do retângulo e aperte barra de espaço             ##
+##   Faça em um fundo branco                                                   ##
+#################################################################################
+
+import cv2
+import numpy as np
+from keras.models import load_model
+
+image_x, image_y = 64, 64
+
+classifier = load_model('./nosso projeto/modelete.h5')
+
+classes = 9
+letras = ['C', 'E', 'F', 'I', 'M', 'N', 'O', 'R', 'S']
+
+
+def predictor(image):  # Faz a predição com a imagem cropada cinza normalizada
+    image = np.array(image, dtype='float32')
+    image /= 255
+    pred_array = classifier.predict(image)
+    print(pred_array)
+    print(np.argmax(pred_array))
+    result = letras[np.argmax(pred_array)]
+    return result
+
+
+cam = cv2.VideoCapture(0)
+
+img_text = ['', '']
+while True:
+    ret, frame = cam.read()
+    frame = cv2.flip(frame, 1)
+
+    img = cv2.rectangle(frame, (425, 100), (625, 300), (0, 255, 0), thickness=2, lineType=8, shift=0)
+
+    imcrop = img[102:298, 427:623]
+
+    # Cria uma imagem de fundo branco 150x150 que servirá para printar a predição
+    output = np.ones((150, 150, 3)) * 255
+    cv2.putText(output, str(img_text), (15, 130), cv2.FONT_HERSHEY_PLAIN, 6, (255, 0, 0))
+
+    # Abertura de janelas de interesse
+    cv2.imshow("ZOOM", imcrop)
+    cv2.imshow("FRAME", frame)
+    cv2.imshow("PREDICTION", output)
+    imggray = cv2.cvtColor(imcrop, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("GRAY SCALE ZOOM", imggray)
+
+    k = cv2.waitKey(10)
+    if k == 32:  # Aperte barra de espaço para realizar a predição
+        frame = np.stack((imggray,) * 1, axis=-1)
+        frame = cv2.resize(imggray, (64, 64))
+        frame = frame.reshape(1, 64, 64, 1)
+        img_text = predictor(frame)
+
+    print(str(img_text))
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
